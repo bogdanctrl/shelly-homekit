26c41aee3183e87776fe6f57dee94cc3ee36b20b
bogdanctrl <bogdanfrincu@icloud.com>
2026-05-27 19:39:43 +0300

    Update shelly_hap_switch.cpp
    Changed input state to visibile in homekit during switch activation

--- src/shelly_hap_switch.cpp
+++ src/shelly_hap_switch.cpp
@@ -48,15 +48,42 @@ Status Switch::Init() {
       iid++, &kHAPCharacteristicType_On,
       [this](HAPAccessoryServerRef *, const HAPBoolCharacteristicReadRequest *,
              bool *value) {
-        *value = out_->GetState() ^ cfg_->hk_state_inverted;
+        *value = in_->GetState() ^ cfg_->hk_state_inverted;
         return kHAPError_None;
       },
       true /* supports_notification */,
-      [this](HAPAccessoryServerRef *, const HAPBoolCharacteristicWriteRequest *,
-             bool value) {
-        SetOutputState(value ^ cfg_->hk_state_inverted, "HAP");
-        return kHAPError_None;
-      },
+     [this](HAPAccessoryServerRef *,
+       const HAPBoolCharacteristicWriteRequest *,
+       bool value) {
+
+    bool current_state = in_->GetState();
+
+    if (value && !current_state) {
+        SetOutputState(true, "HAP");
+        mgos_set_timer(
+            300,
+            0,
+            [](void *arg) {
+                auto sw = (Switch *) arg;
+                sw->SetOutputState(false, "HAP");
+            },
+            this);
+    }
+
+    if (!value && current_state) {
+        SetOutputState(true, "HAP");
+        mgos_set_timer(
+            3000,
+            0,
+            [](void *arg) {
+                auto sw = (Switch *) arg;
+                sw->SetOutputState(false, "HAP");
+            },
+            this);
+    }
+
+    return kHAPError_None;
+},
       kHAPCharacteristicDebugDescription_On);
   state_notify_chars_.push_back(on_char);
   AddChar(on_char);
